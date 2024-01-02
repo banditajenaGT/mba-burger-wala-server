e62f30f3f488a6c9ee953261b38bd7c45d78f6a0
banditajenaGT <banditajena2001@gmail.com>
2024-01-02 20:26:04 +0530

    fixed 3

--- routes/user.js
+++ routes/user.js
@@ -14,7 +14,7 @@ router.get("/login", passport.authenticate("google", {
 }))
 
 router.get("/me", isAuthenticated, myProfile)
-router.get("/logout", logout)
+router.delete("/logout", logout)
 
 //admin middleware
 router.get("/admin/users", isAuthenticated, authorizedAdmin, getAdminUsers)
