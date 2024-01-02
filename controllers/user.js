2a6d12289a311b3e0385069d3c46177afb933c08
banditajenaGT <banditajena2001@gmail.com>
2024-01-02 18:37:57 +0530

    fixed 2

--- controllers/user.js
+++ controllers/user.js
@@ -13,7 +13,7 @@ export const logout = asyncError(async (req, res, next) => {
     req.session.destroy((err) => {
         if (err) return next(err)
 
-        res.setCookie("connect.sid", null, {
+        res.cookie("connect.sid", null, {
             secure: process.env.NODE_ENV === "development" ? false : true,
             httpOnly: process.env.NODE_ENV === "development" ? false : true,
             sameSite: process.env.NODE_ENV === "development" ? false : "none"
